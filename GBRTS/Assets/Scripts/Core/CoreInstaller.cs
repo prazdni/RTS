33b8f336b5740a7937f9935f26689e1e929c73a9
Danil <danilstasilevich@yandex.ru>
2021-06-18 01:48:29 +0300

    Minor changes

--- GBRTS/Assets/Scripts/Core/CoreInstaller.cs
+++ GBRTS/Assets/Scripts/Core/CoreInstaller.cs
@@ -1,14 +1,19 @@
-﻿using Core.Unit;
+﻿using Abstractions;
+using Core.Unit;
+using UnityEngine;
 using Zenject;
 
 namespace Core
 {
     public class CoreInstaller : MonoInstaller
     {
+        [SerializeField] private GameStatus _gameStatus;
+        
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
             Container.BindInterfacesAndSelfTo<AttackCommandExecutor>().FromComponentsInHierarchy().AsTransient();
+            Container.Bind<IGameStatus>().FromInstance(_gameStatus);
         }
     }
 }
